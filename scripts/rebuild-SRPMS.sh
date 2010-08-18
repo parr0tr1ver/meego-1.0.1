c37c2200f7539925feb6003def7ca49078046622
root <root@builder2.redflag-linux.com>
2010-08-18 08:48:46 +0800

    finish rebuild-SRPMS.sh

--- scripts/rebuild-SRPMS.sh
+++ scripts/rebuild-SRPMS.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/sh 
+
+set -x
 
 function usage() 
 {
@@ -6,5 +8,24 @@ function usage()
 	exit 1
 }
 
-pushd $1
-#if [ ! -e '
+if [ $# -lt 1 ]; then 
+	usage
+fi
+
+for para in "$*"; do
+	echo $para
+	pushd $para
+
+	build_time=`date +%s`
+	result_dir=finished.$build_time
+	mkdir $result_dir
+
+	for srpm in `ls *.src.rpm`; do
+		rpmbuild --target i586 --clean --rebuild $srpm &> build_log.$build_time
+		if [ $? -eq 0 ]; then
+			mv $srpm $result_dir
+		fi
+	done
+
+	popd
+done
